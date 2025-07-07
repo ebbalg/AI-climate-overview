f59b45fd8813b280d3b385ffbdb349f31fff6397
ebbalg <161567380+ebbalg@users.noreply.github.com>
2025-07-07 16:03:45 +0200

    feat(gui): update design, layout and navigation

---
+++ app/energy_use_page.py
@@ -0,0 +1,71 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.markdown("""
+    <style>
+        body {
+            background-color: #F7FFF7;
+        }
+        .block-container {
+            padding-top: 2rem;
+        }
+        .info-card {
+            background-color: white;
+            border-radius: 12px;
+            padding: 20px;
+            box-shadow: 0 2px 10px rgba(0,0,0,0.05);
+            text-align: left;
+        }
+        .info-card h4 {
+            margin-bottom: 10px;
+        }
+        .info-card p {
+            margin-bottom: 20px;
+        }
+
+        /* Style Streamlit buttons */
+        div.stButton > button:first-child {
+            background-color: #66ccff;
+            color: black;
+            border: none;
+            border-radius: 8px;
+            padding: 8px 20px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+        div.stButton > button:first-child:hover {
+            background-color: #4dc3ff;
+        }
+
+        .small-card {
+            background-color: #f9f9f9;
+            border-radius: 10px;
+            padding: 20px;
+            height: 160px;
+        }
+        .small-card h5 {
+            margin: 0 0 10px 0;
+        }
+
+        /* Back button styling */
+        .stButton>button.back {
+            background-color: #66ccff;
+            color: black;
+            border: none;
+            border-radius: 6px;
+            padding: 6px 15px;
+            font-size: 16px;
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Back button
+top_col1, _, _ = st.columns([0.1, 0.8, 0.1])
+with top_col1:
+    if st.button("Back", key="back"):
+        st.switch_page("general_overview_page.py")
+
+
+st.markdown('<h1 style="text-align: left;"> Energy Consumption </h1>', unsafe_allow_html=True)
+
