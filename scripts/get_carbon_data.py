0c52879c382e670cfe2da0b39ead695dcc701b09
Kajsa Lidin <104261817+Kajlid@users.noreply.github.com>
2025-07-09 15:49:46 +0200

    Feature/get carbon intensity data (#11)
    * feat(api): add script for retrieving current country specific data from nowtricity
    
    * fix(api): convert carbon intensity factor to g co2eq/wh
    
    * fix: remove double conversions of carbon intensity factor and write informative comments
    
    * feat(ui): retrieve real values from the api for the ui

---
+++ scripts/get_carbon_data.py
@@ -0,0 +1,61 @@
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+def get_carbon_factor(country):
+    """Get the carbon intensity factor and which date the value is from (according to Nowtricity).
+
+    Args:
+        country (string): the country which the user has inputted.
+
+    Returns:
+        _dict_: a dictorinary with the current carbon intensity in the country [g Co2eq / kWh] and the date in UTC time.
+    """
+    
+    country = country.strip().lower()  # ensure standardized format
+    
+    url = f"https://www.nowtricity.com/api/current-emissions/{country}/"
+    
+    api_key = os.getenv("NOWTRICITY_API_KEY")
+    
+    if not api_key:
+        raise ValueError("API key not found. Make sure NOWTRICITY_API_KEY is set in your .env file.")
+    
+    headers = {
+    "X-Api-Key": api_key
+    }
+    
+    try:
+        # GET request
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        
+        response = response.json()
+        
+        carbon_intensity = response["emissions"]["value"]
+        date_utc = response["emissions"]["dateUTC"]
+        
+        return {
+            "carbon_intensity": carbon_intensity,
+            "date_utc": date_utc
+        }
+        
+        
+    except requests.exceptions.HTTPError as err:
+        print(f"HTTP error occurred: {err}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
+    return None
+        
+
+if __name__== "__main__":
+    response_dict = get_carbon_factor("Sweden")
+    if response_dict:
+        print(f"{response_dict["carbon_intensity"]} g CO₂eq/Wh")
+        print(f"UTC Date: {response_dict["date_utc"]}")
+        
+    else:
+        print("Failed to fetch data.")
