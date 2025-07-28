32642974a93d1ecd29086acb39ca8207184480da
Kajlid <104261817+Kajlid@users.noreply.github.com>
2025-07-28 10:26:16 +0200

    feat(gui): add possibility to evaluate image generation and classification

--- app/project_query_page.py
+++ app/project_query_page.py
@@ -127,7 +127,7 @@ with st.form("my_form"):
         value=st.session_state.challenge
     )
     
-    ai_options = ["Text generation", "Text classification", "Speech recognition"]
+    ai_options = ["Text generation", "Text classification", "Speech recognition", "Image generation", "Image classification"]
     ai_function = st.radio(
     "Which AI functionalities might help tackle this problem?",
     ai_options,
